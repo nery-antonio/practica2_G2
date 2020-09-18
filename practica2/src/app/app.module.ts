238ede7c59c06e39537394e90914f5a8c1402792
Abner  Cardona <abner.cardona1997@gmail.com>
2020-09-17 23:49:39 -0600

    Creación de componente login.
    Se crea el componente del login y se diseña su interfaz gráfica.

--- practica2/src/app/app.module.ts
+++ practica2/src/app/app.module.ts
@@ -7,13 +7,16 @@ import { RegisterComponent } from './components/register/register.component';
 import { FormsModule} from '@angular/forms';
 import { RegisterServiceService } from './services/register/register-service.service';
 import { CrearsubscripcionComponent } from './components/crearsubscripcion/crearsubscripcion.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    CrearsubscripcionComponent
+    CrearsubscripcionComponent,
+    LoginComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -25,3 +28,4 @@ import { CrearsubscripcionComponent } from './components/crearsubscripcion/crear
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
