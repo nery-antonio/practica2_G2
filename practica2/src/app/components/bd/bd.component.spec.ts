0d1b32e15e981cca5fdc07c2987f1e5345220aba
Ram <ramiromateo88@gmail.com>
2020-09-20 06:08:40 -0600

    Se agrego prueba unitaria final Mocks
    Se agrego la ultima prueba unitaria  mocks con Spies

--- practica2/src/app/components/bd/bd.component.spec.ts
+++ practica2/src/app/components/bd/bd.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BdComponent } from './bd.component';
+import { MostrarusuariosService } from '../../services/mostrarusuarios/mostrarusuarios.service';
 
 describe('BdComponent', () => {
   let component: BdComponent;
   let fixture: ComponentFixture<BdComponent>;
+  let mockdbservices;
 
   beforeEach(async () => {
+    mockdbservices = jasmine.createSpyObj(['getUser','deleteuser','isAdmin','existoneadmin','isemptybase']);
+    mockdbservices.getUser.and.returnValue([]);
+    mockdbservices.deleteuser.and.returnValue(true);
+    mockdbservices.isAdmin.and.returnValue(true);
+    mockdbservices.existoneadmin.and.returnValue(true);
+    mockdbservices.isemptybase.and.returnValue(true);
+    
     await TestBed.configureTestingModule({
       declarations: [ BdComponent ]
+      
     })
     .compileComponents();
   });
@@ -23,6 +33,13 @@ describe('BdComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('Se encuentra vacia la base de datos',()=>{
+    expect(mockdbservices.isemptybase()).toBeTruthy();
+    mockdbservices.isemptybase = jasmine.createSpy().and.returnValue(false);
+    expect(mockdbservices.isemptybase()).toBeFalsy();
+  });
+
+
   it('La lista que se recibe de base de datos no debe ser null', ()=>{
     expect(component.getUserfromback()).not.toBeNull();
   })
@@ -30,16 +47,13 @@ describe('BdComponent', () => {
   it('No se puede eliminar un usuario de tipo administrador', ()=>{
     let correo_admin='admin@gmail.com'
     expect(component.deleteuser(correo_admin)).toBeFalse();
-  })
+  });
 
-  it('No se puede eliminar un usuario que no existe', ()=>{
-    let correo_admin='usernews@gmail.com'
-    expect(component.deleteuser(correo_admin)).toBeFalse();
-  })
+  
 
   it('Eliminado usuario no administrador', ()=>{
     let correo_admin='abner@gmail.com'
     expect(component.deleteuser(correo_admin)).toBeTrue();
-  })
+  });
 
 });
